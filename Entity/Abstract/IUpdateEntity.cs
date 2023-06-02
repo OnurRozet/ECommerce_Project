d5604a9283401c0d294f2f98cd66604c52728c68
Onur <onurozet@hotmail.com>
2023-06-02 18:18:02 +0300

    Entities created

---
+++ Entity/Abstract/IUpdateEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity.Abstract
+{
+	public interface IUpdateEntity
+	{
+		int? UpdatedUserId { get; set; }
+		DateTime? UpdateDate { get; set; }
+	}
+}
---
+++ Entity/Concrete/BaseEntities/AuditableEntity.cs
@@ -0,0 +1,18 @@
+﻿using Entity.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity.Concrete.BaseEntities
+{
+	public class AuditableEntity : BaseEntity, ICreatedEntity, IUpdateEntity
+	{
+		public int CreatedUserId { get; set; }
+		public DateTime CreatedDate { get; set; }
+		public int? UpdatedUserId { get; set; }
+		public DateTime? UpdateDate { get; set; }
+		
+	}
+}
