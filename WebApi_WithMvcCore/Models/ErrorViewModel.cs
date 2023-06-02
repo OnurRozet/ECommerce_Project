dc2113a5a40c5e12b3c9d433130fa76b28de2d4f
Onur <onurozet@hotmail.com>
2023-06-02 16:45:25 +0300

    Add project files.

---
+++ WebApi_WithMvcCore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebApi_WithMvcCore.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
